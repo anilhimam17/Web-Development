3bf83cff05bbdddc0b39f737f4cfea7535b23569
Godugu Anil Himam <63832213+anilhimam17@users.noreply.github.com>
2024-03-24 13:20:54 +0000

    Using the React Framework
    Worked on
    - React Components
    - JSX Expression
    - React Dir Structure

---
+++ React Development/React Components/src/Heading.jsx
@@ -0,0 +1,10 @@
+// Importing React for JSX functionality
+import React from "react";
+
+// JSX Component
+function Heading() {
+  return <h1>Hello, World from JSX Components</h1>;
+}
+
+// Exporting the JSX Component to be made available in Index.js
+export default Heading;
