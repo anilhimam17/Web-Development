93be5f942d1867293e7c0431f065b396d1f4c6f6
Godugu Anil Himam <63832213+anilhimam17@users.noreply.github.com>
2024-09-21 23:36:56 +0100

    Re-Ordered the Directory Structure.

--- React Development/React Components Practice/src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Greeting from "./Greeting";
-
-function App() {
-  return <Greeting />;
-}
-
-// Exporting the App
-export default App;
--- React Development/React Components/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.jsx";
-
-// Since the render function is injecting code into a div <App /> doesnt require a div anymore
-ReactDOM.render(<App />, document.getElementById("root"));
